326bc8eb2cea686b039b0f069a354448a4f7fc9b
Jonathan Minton <nate.minton@gmail.com>
2015-05-25 14:24:30 +0100

    additional figures - smoothed age group and  cohort scatterplots

--- script.r
+++ script.r
@@ -16,195 +16,12 @@ require(spatstat)
 require(RColorBrewer)
 require(ggplot2)
 require(lattice)
+require(latticeExtra)
 
 
+# Functions ---------------------------------------------------------------
 
-
-# Data --------------------------------------------------------------------
-
-
-data <- read.csv("data/real/scotland_all.csv") %>%
-  tbl_df
-
-names(data) <- c(
-  "country",
-  "year",
-  "age",
-  "sex",
-  "convicted",
-  "total"
-  )
-
-
-# This converts the values in the sex vector to lowercase
-data$sex <- tolower(data$sex)
-data$age <- revalue(data$age, c("90 & over" = "90"))
-data$age <- as.numeric(as.character(data$age))
-
-data <- data  %>% mutate(convict_rate = convicted/total)
-
-data_younger <- data  %>% 
-  filter(age <= 60)
-
-
-data_mfratio <- data_younger  %>% 
-  tbl_df   %>% 
-  select(-convicted, -total) %>% 
-  spread(key=sex, value=convict_rate)  %>% 
-  mutate(mf_ratio = male/female)
-
-
-# Contour plots -----------------------------------------------------------
-
-png(filename="figures/all_scotland.png", 
-    width=40, height=20, res=300, units="cm"
-)
-# Let's look at the mort rates only
-contourplot(
-  convict_rate ~ year * age | sex, 
-  data=data, 
-  region=T, 
-  par.strip.text=list(cex=1.4, fontface="bold"),
-  ylab=list(label="Age in years", cex=1.4),
-  xlab=list(label="Year", cex=1.4),
-  cex=1.4,
-  cuts=20,
-  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
-  main=NULL,
-  labels=list(cex=1.2),
-  col="blue",
-  scales=list(
-    x=list(cex=1.4), 
-    y=list(cex=1.4),
-    alternating=3
-  )
-)
-dev.off()
-
-
-####################################################################
-
-
-png(filename="figures/all_scot_younger.png", 
-    width=40, height=20, res=300, units="cm"
-)
-# Let's look at the mort rates only
-contourplot(
-  convict_rate ~ year * age | sex, 
-  data=data_younger, 
-  region=T, 
-  par.strip.text=list(cex=1.4, fontface="bold"),
-  ylab=list(label="Age in years", cex=1.4),
-  xlab=list(label="Year", cex=1.4),
-  cex=1.4,
-  cuts=20,
-  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
-  main=NULL,
-  labels=list(cex=1.2),
-  col="blue",
-  scales=list(
-    x=list(cex=1.4), 
-    y=list(cex=1.4),
-    alternating=3
-  )
-)
-dev.off()
-
-
-# males and females separately
-
-
-# identity scale, younger ages 
-png(filename="figures/male_scot_younger.png", 
-    width=23, height=20, res=300, units="cm"
-)
-# Let's look at the mort rates only
-contourplot(
-  convict_rate ~ year * age, 
-  data=subset(data_younger, subset=sex=="male"), 
-  region=T, 
-  par.strip.text=list(cex=1.4, fontface="bold"),
-  ylab=list(label="Age in years", cex=1.4),
-  xlab=list(label="Year", cex=1.4),
-  cex=1.4,
-  cuts=20,
-  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
-  main=NULL,
-  labels=list(cex=1.2),
-  col="blue",
-  scales=list(
-    x=list(cex=1.4), 
-    y=list(cex=1.4),
-    alternating=3
-  )
-)
-dev.off()
-
-
-png(filename="figures/female_scot_younger.png", 
-    width=23, height=20, res=300, units="cm"
-)
-# Let's look at the mort rates only
-contourplot(
-  convict_rate ~ year * age, 
-  data=subset(data_younger, subset=sex=="female"), 
-  region=T, 
-  par.strip.text=list(cex=1.4, fontface="bold"),
-  ylab=list(label="Age in years", cex=1.4),
-  xlab=list(label="Year", cex=1.4),
-  cex=1.4,
-  cuts=20,
-  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
-  main=NULL,
-  labels=list(cex=1.2),
-  col="blue",
-  scales=list(
-    x=list(cex=1.4), 
-    y=list(cex=1.4),
-    alternating=3
-  )
-)
-dev.off()
-
-
-## Difference between males and females
-
-
-# This looks at the difference between males and females in crime rates
-# at different ages and in different years
-
-# this creates a subset of the data using only the variables of interest
-
-
-
-png("figures/mf_ratio.png",
-    width=23, height=20, res=300, units="cm"
-)
-contourplot(
-  mf_ratio ~ year * age, 
-#  data=subset(data_mfratio, age <=50), 
-  data=subset(mf_ratio_blurred, age <=50),
-  region=T, 
-  par.strip.text=list(cex=1.4, fontface="bold"),
-  ylab=list(label="Age in years", cex=1.4),
-  xlab=list(label="Year", cex=1.4),
-  cex=1.4,
-  at=1:10,
-  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
-  main=NULL,
-  labels=list(cex=1.2),
-  col="blue",
-  scales=list(
-    x=list(cex=1.4), 
-    y=list(cex=1.4),
-    alternating=3
-  )
-)
-dev.off()
-
-# Smooth
-
-fn <- function(input, smooth_par=2){
+smooth <- function(input, smooth_par=2){
   
   dta <- input %>%
     select(year, age, mf_ratio) %>%
@@ -235,7 +52,198 @@ fn <- function(input, smooth_par=2){
   return(output)
 }
 
-mf_ratio_blurred <- fn(data_mfratio, smooth_par=1.5)
+
+
+# Data --------------------------------------------------------------------
+
+
+data <- read.csv("data/real/scotland_all.csv") %>%
+  tbl_df
+
+names(data) <- c(
+  "country",
+  "year",
+  "age",
+  "sex",
+  "convicted",
+  "total"
+  )
+
+
+# This converts the values in the sex vector to lowercase
+data$sex <- tolower(data$sex)
+data$age <- revalue(data$age, c("90 & over" = "90"))
+data$age <- as.numeric(as.character(data$age))
+
+data <- data  %>% 
+  mutate(convict_rate = convicted/total) %>%
+  mutate(cohort = year - age) %>%
+  arrange(sex, cohort, age)
+
+data_younger <- data  %>% 
+  filter(age <= 60) 
+  
+data_mfratio <- data_younger  %>% 
+  tbl_df   %>% 
+  select(-convicted, -total) %>% 
+  spread(key=sex, value=convict_rate)  %>% 
+  mutate(mf_ratio = male/female)
+
+data_mfratio_smoothed <- smooth(data_mfratio, 1.5)
+
+# Contour plot - all ages, both sexes  ------------------------------------------------
+
+png(filename="figures/all_scotland.png", 
+    width=40, height=20, res=300, units="cm"
+)
+# Let's look at the mort rates only
+contourplot(
+  convict_rate ~ year * age | sex, 
+  data=data, 
+  region=T, 
+  par.strip.text=list(cex=1.4, fontface="bold"),
+  ylab=list(label="Age in years", cex=1.4),
+  xlab=list(label="Year", cex=1.4),
+  cex=1.4,
+  cuts=20,
+  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
+  main=NULL,
+  labels=list(cex=1.2),
+  col="blue",
+  scales=list(
+    x=list(cex=1.4), 
+    y=list(cex=1.4),
+    alternating=3
+  )
+)
+dev.off()
+
+
+# Contour plot: younger ages, both sexes ----------------------------------
+
+png(filename="figures/all_scot_younger.png", 
+    width=40, height=20, res=300, units="cm"
+)
+# Let's look at the mort rates only
+contourplot(
+  convict_rate ~ year * age | sex, 
+  data=data_younger, 
+  region=T, 
+  par.strip.text=list(cex=1.4, fontface="bold"),
+  ylab=list(label="Age in years", cex=1.4),
+  xlab=list(label="Year", cex=1.4),
+  cex=1.4,
+  cuts=20,
+  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
+  main=NULL,
+  labels=list(cex=1.2),
+  col="blue",
+  scales=list(
+    x=list(cex=1.4), 
+    y=list(cex=1.4),
+    alternating=3
+  )
+)
+dev.off()
+
+
+
+# contour plot: younger ages, males ---------------------------------------
+
+
+# identity scale, younger ages 
+png(filename="figures/male_scot_younger.png", 
+    width=23, height=20, res=300, units="cm"
+)
+# Let's look at the mort rates only
+contourplot(
+  convict_rate ~ year * age, 
+  data=subset(data_younger, subset=sex=="male"), 
+  region=T, 
+  par.strip.text=list(cex=1.4, fontface="bold"),
+  ylab=list(label="Age in years", cex=1.4),
+  xlab=list(label="Year", cex=1.4),
+  cex=1.4,
+  cuts=20,
+  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
+  main=NULL,
+  labels=list(cex=1.2),
+  col="blue",
+  scales=list(
+    x=list(cex=1.4), 
+    y=list(cex=1.4),
+    alternating=3
+  )
+)
+dev.off()
+
+
+# contour plot: younger ages, females -------------------------------------
+
+png(filename="figures/female_scot_younger.png", 
+    width=23, height=20, res=300, units="cm"
+)
+# Let's look at the mort rates only
+contourplot(
+  convict_rate ~ year * age, 
+  data=subset(data_younger, subset=sex=="female"), 
+  region=T, 
+  par.strip.text=list(cex=1.4, fontface="bold"),
+  ylab=list(label="Age in years", cex=1.4),
+  xlab=list(label="Year", cex=1.4),
+  cex=1.4,
+  cuts=20,
+  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200),
+  main=NULL,
+  labels=list(cex=1.2),
+  col="blue",
+  scales=list(
+    x=list(cex=1.4), 
+    y=list(cex=1.4),
+    alternating=3
+  )
+)
+dev.off()
+
+
+# contour plot: male/female ratio -----------------------------------------
+
+
+png("figures/mf_ratio.png",
+    width=23, height=20, res=300, units="cm"
+)
+p1 <- contourplot(
+      mf_ratio ~ year * age, 
+      data=subset(data_mfratio_smoothed, age <=50),
+      cex=1.4,
+      ylab="",
+      xlab="",
+      at=1:10,
+      main=NULL,
+      col="blue",
+      labels=list(cex=1.2),
+      scales=list(NULL)
+)
+
+p2 <- levelplot(
+  mf_ratio ~ year * age, 
+  data = subset(data_mfratio, age <=50),
+  region=T,
+  ylab=list(label="Age in years", cex=1.4),
+  xlab=list(label="Year", cex=1.4),
+  scales=list(
+    x=list(cex=1.4), 
+    y=list(cex=1.4),
+    alternating=3
+  ),
+  col.regions=colorRampPalette(brewer.pal(6, "Greys"))(200)
+)
+
+print(p2 + p1)
+dev.off()
+
+# Smooth
+
 # STL files ---------------------------------------------------------------
 
 
@@ -329,9 +337,7 @@ ggsave(
 
 # This creates a birth cohort variable and adds it to the data_younger dataset
 
-data_younger <- data_younger %>%
-  mutate(cohort = year - age) %>%
-  arrange(sex, cohort, age)
+
 
 
 data_younger %>%
@@ -372,10 +378,7 @@ ggsave(
 )
 
 
-# New figs ----------------------------------------------------------------
-
-
-# This figure will show changes in the shape of the age-crime curve over time
+# age- shape and scale curve: shape --------------------------------
 
 data_younger  %>% 
   select(-country)  %>% 
@@ -401,7 +404,33 @@ ggsave(
   width=12, height=12, unit="cm", dpi=300
 )
 
-# This figure will show changes in the scale of the age-crime curve over time
+# with smoother
+data_younger  %>% 
+  select(-country)  %>% 
+  group_by(year, sex)  %>% 
+  arrange(year, sex, age)  %>% 
+  mutate(
+    rel_convict_rate = convict_rate / max(convict_rate),
+    year_group = cut(
+      year, 
+      breaks=c(1989, 1995, 2000, 2005, 2012),
+      include.lowest=T, right=T
+    )
+  ) %>%
+  ggplot(data=.) +
+  geom_point(aes(x=age, y=rel_convict_rate, group=cohort), alpha=0.2) + 
+  stat_smooth(aes(x=age, y=rel_convict_rate, group=year_group, colour=year_group)) + 
+  facet_wrap(~ sex)  + 
+  labs(x="Age in years", y="Relative conviction rate", colour="Year range") +
+  guides(colour=guide_legend(nrow=2)) +
+  theme(legend.position="bottom")
+
+ggsave(
+  "figures/relative_agecrime_smoothed.png", 
+  width=12, height=12, unit="cm", dpi=300
+)
+
+# age- shape and scale curve: scale --------------------------------
 
 data_younger  %>% 
   select(-country)  %>% 
@@ -419,5 +448,84 @@ data_younger  %>%
 
 ggsave(
   "figures/max_agecrime.png", 
+  width=12, height=8, unit="cm", dpi=300
+)
+
+
+# cohort- shape and scale curve: shape --------------------------------
+
+data_younger  %>% 
+  select(-country)  %>% 
+  group_by(cohort, sex)  %>% 
+  arrange(cohort, sex, age)  %>%
+  filter(cohort >=1970 & cohort <= 1985) %>%
+  mutate(
+    rel_convict_rate = convict_rate / max(convict_rate),
+    cohort_group = cut(
+      cohort, 
+      breaks=c(1970, 1975, 1980, 1985),
+      include.lowest=T, right=T
+    )
+  ) %>%
+  ggplot(data=.) +
+  geom_line(aes(x=age, y=rel_convict_rate, group=cohort, colour=cohort_group), alpha=0.5) + 
+  facet_wrap(~ sex)  + 
+  labs(x="Age in years", y="Relative conviction rate", colour="Cohort range") +
+  guides(colour=guide_legend(nrow=2)) +
+  theme(legend.position="bottom")
+
+ggsave(
+  "figures/relative_agecrime_cohort.png", 
+  width=12, height=12, unit="cm", dpi=300
+)
+
+# with smoother? ----------------------------------------------------------
+
+data_younger  %>% 
+  select(-country)  %>% 
+  group_by(cohort, sex)  %>% 
+  arrange(cohort, sex, age)  %>%
+  filter(cohort >=1970 & cohort <= 1985) %>%
+  mutate(
+    rel_convict_rate = convict_rate / max(convict_rate),
+    cohort_group = cut(
+      cohort, 
+      breaks=c(1970, 1975, 1980, 1985),
+      include.lowest=T, right=T
+    )
+  ) %>%
+  ggplot(data=.) +
+  geom_point(aes(x=age, y=rel_convict_rate, group=cohort), alpha=0.2) + 
+  stat_smooth(aes(x=age, y=rel_convict_rate, group=cohort_group, colour=cohort_group)) + 
+  facet_wrap(~ sex)  + 
+  labs(x="Age in years", y="Relative conviction rate", colour="Cohort range") +
+  guides(colour=guide_legend(nrow=2)) +
+  theme(legend.position="bottom")
+
+ggsave(
+  "figures/relative_agecrime_smoothed_cohort.png", 
+  width=12, height=12, unit="cm", dpi=300
+)
+
+
+# age- shape and scale curve: scale --------------------------------
+
+data_younger  %>% 
+  select(-country)  %>% 
+  filter(cohort >=1970 & cohort <= 1985) %>%
+  group_by(sex, cohort)  %>% 
+  summarise(
+    max_convict_rate =  max(convict_rate)
+  ) %>% 
+  ggplot(data=.) +
+  geom_line(aes(x=cohort, y=max_convict_rate)) + 
+  facet_wrap(~ sex, scales="free")  + 
+  labs(x="Birth cohort", y="Maximum age-specific \nconviction rate") +
+  theme(
+    axis.text.x = element_text(angle =90, hjust=1, vjust=0.5)
+  )
+
+ggsave(
+  "figures/max_agecrime_cohort.png", 
   width=12, height=8, unit="cm", dpi=300
 )
