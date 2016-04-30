af84600f2389bcaea87ddb3c9aec1a5a11c79014
Jonathan Minton <nate.minton@gmail.com>
2016-04-30 14:54:53 +0100

    produce some models for seeing if the shape of the age crime curve has changed over time

--- script.r
+++ script.r
@@ -599,3 +599,102 @@ tmp <- data_younger %>%
       family=Gamma(link="identity")
       )
 
+
+
+# Modelling of differences over time  -------------------------------------
+
+
+data
+
+# Interested in proportion of convictions 'owned' 
+# by any particular group in any year 
+
+model_data <- data %>% 
+  group_by(year) %>% 
+  mutate(prop_convicted = convicted / sum(convicted)) %>%
+  select(year, age, sex, prop_convicted) %>%
+  arrange(year, sex, age)
+
+
+lm(prop_convicted ~ year, model_data) %>% AIC() -> a_year
+lm(prop_convicted ~ sex, model_data) %>% AIC() -> a_sex
+lm(prop_convicted ~ age, model_data) %>% AIC() -> a_age
+
+# Of these three simplest models, the age model has the lowest AIC
+
+# Now two way linear models
+
+lm(prop_convicted ~ age + year, model_data) %>% AIC() -> a_age_year
+lm(prop_convicted ~ age + sex, model_data) %>% AIC() -> a_age_sex
+
+# of these two, age + sex beats age + year
+
+# But we know age does not have a linear effect - how many polynomials 
+# is appropriate?
+
+# without sex interaction
+lm(prop_convicted ~ poly(age,2)  + sex, model_data) %>% AIC() -> a_age2_sex
+lm(prop_convicted ~ poly(age,3)  + sex, model_data) %>% AIC() -> a_age3_sex
+lm(prop_convicted ~ poly(age,4)  + sex, model_data) %>% AIC() -> a_age4_sex
+lm(prop_convicted ~ poly(age,5)  + sex, model_data) %>% AIC() -> a_age5_sex
+lm(prop_convicted ~ poly(age,6)  + sex, model_data) %>% AIC() -> a_age6_sex
+lm(prop_convicted ~ poly(age,7)  + sex, model_data) %>% AIC() -> a_age7_sex
+lm(prop_convicted ~ poly(age,8)  + sex, model_data) %>% AIC() -> a_age8_sex
+lm(prop_convicted ~ poly(age,9)  + sex, model_data) %>% AIC() -> a_age9_sex
+lm(prop_convicted ~ poly(age,10)  + sex, model_data) %>% AIC() -> a_age10_sex
+
+lm(prop_convicted ~ poly(age,1)  * sex, model_data) %>% AIC() -> a_age1sex
+lm(prop_convicted ~ poly(age,2)  * sex, model_data) %>% AIC() -> a_age2sex
+lm(prop_convicted ~ poly(age,3)  * sex, model_data) %>% AIC() -> a_age3sex
+lm(prop_convicted ~ poly(age,4)  * sex, model_data) %>% AIC() -> a_age4sex
+lm(prop_convicted ~ poly(age,5)  * sex, model_data) %>% AIC() -> a_age5sex
+lm(prop_convicted ~ poly(age,6)  * sex, model_data) %>% AIC() -> a_age6sex
+lm(prop_convicted ~ poly(age,7)  * sex, model_data) %>% AIC() -> a_age7sex
+lm(prop_convicted ~ poly(age,8)  * sex, model_data) %>% AIC() -> a_age8sex
+lm(prop_convicted ~ poly(age,9)  * sex, model_data) %>% AIC() -> a_age9sex
+lm(prop_convicted ~ poly(age,10)  * sex, model_data) %>% AIC() -> a_age10sex
+
+
+tmp <- data.frame(poly = 1:10, 
+                  interaction = rep(c(F, T), each = 10),
+                  aic = 
+                    c(
+      a_age_sex, a_age2_sex, a_age3_sex, a_age4_sex, a_age5_sex,
+      a_age6_sex, a_age7_sex, a_age8_sex, a_age9_sex, a_age10_sex,
+      a_age1sex, a_age2sex, a_age3sex, a_age4sex, a_age5sex,
+      a_age6sex, a_age7sex, a_age8sex, a_age9sex, a_age10sex
+      
+                      )
+      )
+
+qplot(x = as.factor(poly), y = aic, group = interaction, colour = interaction, data = tmp)
+
+
+best_invariant_model <- lm(prop_convicted ~ poly(age,3)  * sex, model_data) 
+
+?update
+
+model_with_year <- update(best_invariant_model, . ~ . + I(year - 1989))
+
+model_interrupted_complex <- model_data %>% 
+  ungroup() %>% 
+  mutate(year2 = year - 1989) %>%
+  mutate(year3 = ifelse(year - 2000 < 0, 0, year - 2000)) %>% 
+  lm(prop_convicted ~ poly(age,3)  * sex + year2 + year3, data = .)
+
+
+
+model_interrupted_interracted <- model_data %>% 
+  ungroup() %>% 
+  mutate(year2 = year - min(year)) %>%
+  mutate(post_drop = ifelse(year >= 2000, TRUE, FALSE)) %>% 
+  lm(prop_convicted ~ poly(age,3)  * sex * post_drop, data = .)
+
+model_yearandinterrupted_interracted <- model_data %>% 
+  ungroup() %>% 
+  mutate(year2 = year - min(year)) %>%
+  mutate(year3 = ifelse(year >= 2000, year - 2000, 0)) %>% 
+  lm(prop_convicted ~ (poly(age,3)  * sex )* (year2 + year3), data = .)
+
+
+
